f4ce123f4ffe64bd286fb5da5811b50dfb9f9b55
Truong Tran Mai Anh <60804140+maicarem@users.noreply.github.com>
2025-07-17 14:08:48 +0000

    Update rescale function to return output scaled by 10

--- src/example_package_anhtruongg/rescale.py
+++ src/example_package_anhtruongg/rescale.py
@@ -5,4 +5,4 @@ def rescale(input_array):
     low = np.min(input_array) 
     high = np.max(input_array) 
     output_array = (input_array - low) / (high - low) 
-    return output_array
+    return output_array * 10
